36a933d8bf6a79fbe4dba1fa55f0027a6452068a
FVS-MBA <feliven@gmail.com>
2026-09-05 15:18:47 -0300

    implementa Pytest

---
+++ tests/test_api.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "mensagem": "Bem-vindo à API de Recomendação de Produtos"
+    }
+
+
+# def func(x):
+#     return x + 1
+
+
+# def test_answer():
+#     assert func(3) == 5
