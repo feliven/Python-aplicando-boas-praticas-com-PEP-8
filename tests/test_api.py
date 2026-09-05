b97c775ede9756863d9b4471f1070df41652d6f0
FVS-MBA <feliven@gmail.com>
2026-09-05 17:38:02 -0300

    testa rotas de usuário e produtos

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,6 +12,58 @@ def test_home():
     }
 
 
+produto = {"nome": "A", "categoria": "B", "tags": ["C", "D"]}
+
+
+def test_criar_produto():
+    response = client.post("/produtos/", json=produto)
+    json = response.json()
+
+    assert response.status_code == 200
+    assert json["nome"] == produto["nome"]
+    assert json["categoria"] == produto["categoria"]
+    assert json["tags"][0] == produto["tags"][0]
+    assert json["tags"][1] == produto["tags"][1]
+    assert json["id"]
+
+
+def test_listar_produtos():
+    response = client.get("/produtos/")
+    json = response.json()
+
+    assert response.status_code == 200
+    assert len(json) == 1
+    assert json[0]["nome"] == produto["nome"]
+    assert json[0]["categoria"] == produto["categoria"]
+    assert json[0]["tags"][0] == produto["tags"][0]
+    assert json[0]["tags"][1] == produto["tags"][1]
+
+
+usuario1 = {"nome": "A"}
+usuario2 = {"nome": "B"}
+
+
+def test_criar_usuario():
+    response = client.post("/usuarios/", params=usuario1)
+    json = response.json()
+
+    assert response.status_code == 200
+    assert json["nome"] == usuario1["nome"]
+    assert json["id"]
+
+    client.post("/usuarios/", params=usuario2)
+
+
+def test_listar_usuarios():
+    response = client.get("/usuarios/")
+    json = response.json()
+
+    assert response.status_code == 200
+    assert len(json) == 2
+    assert json[0]["nome"] == usuario1["nome"]
+    assert json[1]["nome"] == usuario2["nome"]
+
+
 # def func(x):
 #     return x + 1
 
